b45d6d439243f2d1d9b98eec903aa1ce288e9834
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2024-06-13 08:26:49 -0300

    css para ecomerce

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -3,49 +3,28 @@ const socket = io();
 socket.on('ProductsIo', (data) => {
   updateProductsList(data);
 })
+
 function updateProductsList(productsIo) {
   const productsContainer = document.getElementById('productsContainer');
-
-  // Limpiar la vista actual y agregar título
-  productsContainer.innerHTML = '<h1 class="title">Lista de Productos</h1>';
-
-  // Creo la tabla y el encabezado.
-  const productTable = document.createElement('table');
-  productTable.innerHTML = `
-    <tr>
-      <th>Título</th>
-      <th>Descripción</th>
-      <th>Código</th>
-      <th>Precio</th>
-      <th>Status</th>
-      <th>Stock</th>
-      <th>Categoría</th>
-      <th>Acción</th>
-    </tr>
-  `;
-
-  // Crear el cuerpo de la tabla
-  const tableBody = document.createElement('tbody');
-  productTable.appendChild(tableBody);
-
-  // Iterar sobre cada producto y agregarlo al cuerpo de la tabla
+  // Iterar sobre cada producto y crear su representación en el DOM
   productsIo.forEach(product => {
-    const productRow = document.createElement('tr');
-    productRow.innerHTML = `
-      <td>${product.title}</td>
-      <td>${product.description}</td>
-      <td>${product.code}</td>
-      <td>${product.price}</td>
-      <td>${product.status}</td>
-      <td>${product.stock}</td>
-      <td>${product.category}</td>
-      <td><button type="button" class="btn-primary" onclick="deleteProduct(${product.pid})">Eliminar</button></td>
-    `;
-    tableBody.appendChild(productRow);
+    const productDiv = document.createElement('div');
+    productDiv.innerHTML = `
+   
+    <div class="card">
+    <h2>${product.title}</h2>
+    <p>Descripción: ${product.description}</p>
+    <p>Código: ${product.code}</p>
+    <p>Precio: ${product.price}</p>
+    <p>Status: ${product.status}</p>
+    <p>Stock: ${product.stock}</p>
+    <p>Categoría: ${product.category}</p>
+    <button type="button" class="btn-secundary" onclick="deleteProduct(${product.pid})">
+    Eliminar
+    </button>
+    </div>`;
+    productsContainer.appendChild(productDiv);
   });
-
-  // Agregar la tabla al contenedor
-  productsContainer.appendChild(productTable);
 };
 
  // Escuchar el evento de envío del formulario
